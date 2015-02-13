42b76c38a63dab5df9d0328d1f4464b8afc81e72
panda88888 <panda88888@gmail.com>
2015-02-12 23:49:38 -0800

    Using package stringutil.Reverse to reverse hello world.

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/panda88888/stringutil"
+)
 
 func main() {
-    fmt.Printf("Hello, World!\n")
+    fmt.Printf( stringutil.Reverse("Hello, Go!") )
 }
